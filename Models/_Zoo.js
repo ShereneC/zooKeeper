6cdf85e43d155be1b1ab0ef08667e7c0a5e2aa9a
ShereneC <spyhopper@cableone.net>
2021-07-12 16:16:56 -0600

    attempt - make beneficials class

--- Models/_Zoo.js
+++ Models/_Zoo.js
@@ -5,6 +5,7 @@ import Dragonfly from './Dragonfly.js'
 import Earwig from './Earwig.js'
 import Fly from './Fly.js'
 
+
 let angie = new Aphid('angie', 'green', false, 1, 'high')
 let barney = new Bee('Barney', 'orange', true, 12, 'flowers')
 let cenny = new Centipede('Cenny', 'brown', true, 3)
@@ -12,5 +13,13 @@ let dan = new Dragonfly('Dan', 'blue', true, 40, 'flowers')
 let ernie = new Earwig('Ernie', 'brown', false, 21, 'decaying wood')
 let flippy = new Fly('Flippy', 'black', false, 12)
 
+// NOTE this is not working - trying to create a class that has a method for putting the goodguys in a group.
+// export default class Beneficial {
+//   beneficials = []
+//   beneficials.push(cenny)
+//   console.log(beneficials)
+// }
 
-
+export default class Critters {
+  constructor()
+}
--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-// import Zoo from './Models/_Zoo.js'
+// NOTE having these instances here allows me to console each instance and run the methods that are in the class files.
+// import Aphid from './Models/Aphid.js'
+// import Bee from './Models/Bee.js'
+// import Centipede from './Models/Centipede.js'
+// import Dragonfly from './Models/Dragonfly.js'
+// import Earwig from './Models/Earwig.js'
+// import Fly from './Models/Fly.js'
 
-import Aphid from './Models/Aphid.js'
-import Bee from './Models/Bee.js'
-import Centipede from './Models/Centipede.js'
-import Dragonfly from './Models/Dragonfly.js'
-import Earwig from './Models/Earwig.js'
-import Fly from './Models/Fly.js'
-
-let angie = new Aphid('angie', 'green', false, 1, 'high')
-let barney = new Bee('Barney', 'orange', true, 12, 'flowers')
-let cenny = new Centipede('Cenny', 'brown', true, 3)
-let dan = new Dragonfly('Dan', 'blue', true, 40, 'flowers')
-let ernie = new Earwig('Ernie', 'brown', false, 21, 'decaying wood')
-let flippy = new Fly('Flippy', 'black', false, 12)
+// let angie = new Aphid('angie', 'green', false, 1, 'high')
+// let barney = new Bee('Barney', 'orange', true, 12, 'flowers')
+// let cenny = new Centipede('Cenny', 'brown', true, 3)
+// let dan = new Dragonfly('Dan', 'blue', true, 40, 'flowers')
+// let ernie = new Earwig('Ernie', 'brown', false, 21, 'decaying wood')
+// let flippy = new Fly('Flippy', 'black', false, 12)
 
 // console.log(angie)
 // console.log(barney)
@@ -21,4 +20,10 @@ let flippy = new Fly('Flippy', 'black', false, 12)
 // console.log(ernie)
 // console.log(flippy)
 
-angie.eat('kale')
+// angie.eat('kale')
+// angie.speak('yummm!')
+
+// NOTE Now I am attempting to create a class of my classes - i.e. zoo of goodGuys and zoo of badGuys
+import showBeneficial from './Models/_Zoo.js'
+
+window.zoo = new showBeneficial()
